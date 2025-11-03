8a8495ce05e58bb1ecf58335d23f7efb14820388
Afonso Miguel <afonso.miguel@pucpr.br>
2025-11-03 17:20:33 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use std::thread;
 fn main() {
     let numbers = Arc::new(vec![1,2,3,4,5]);
 
-    thread::spawn(move || {
-        println!("Vetor: {:?}", numbers);
-    });
+    for _ in 0..2{
+        let numbers_clone = Arc::clone(&numbers);
+
+        thread::spawn(move || {
+            println!("Vetor: {:?}", numbers_clone);
+        });
+    }
 
     thread::sleep(Duration::from_secs(1));
 }
