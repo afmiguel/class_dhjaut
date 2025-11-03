0e7b55b30373917b0b00c559d2b6f76a75322777
Afonso Miguel <afonso.miguel@pucpr.br>
2025-11-03 17:03:38 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use std::thread;
 fn main() {
     let numbers = Rc::new(vec![1,2,3,4,5]);
 
-    println!("Vetor: {:?}", numbers);
+    thread::spawn(move ||{
+        println!("Vetor: {:?}", numbers);
+    });
 
     thread::sleep(Duration::from_secs(1));
 }
