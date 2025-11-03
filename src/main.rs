40969186166999fe8f235546441ac28696490686
Afonso Miguel <afonso.miguel@pucpr.br>
2025-11-03 17:09:20 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::rc::Rc;
+use std::sync::Arc;
 use std::time::Duration;
 use std::thread;
 
 fn main() {
-    let numbers = Rc::new(vec![1,2,3,4,5]);
+    let numbers = Arc::new(vec![1,2,3,4,5]);
 
-    thread::spawn(move ||{
+    thread::spawn(move || {
         println!("Vetor: {:?}", numbers);
     });
 
