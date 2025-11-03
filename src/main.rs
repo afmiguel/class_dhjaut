36c17502a320548c4f20c2e7cd3be2b43f464309
Afonso Miguel <afonso.miguel@pucpr.br>
2025-11-03 16:53:25 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
+    let numbers = Rc::new(vec![1,2,3,4,5]);
 
+    println!("Vetor: {:?}", numbers);
+
+    thread::sleep(Duration::from_secs(1));
 }
